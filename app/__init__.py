1aa1f770ea855006b2242a87a002f4b12b54cbae
Luiggi Tenorio <luiggi.tenorio@pucp.pe>
2021-04-15 20:04:36 -0500

    gunicorn added

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask import Blueprint
 from .main.controller.user_controller import api as user_ns
 from .main.controller.login_controller import api as login_ns
 
+import os
+from flask_cors import CORS
+from .main import create_app
+
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
@@ -14,3 +18,10 @@ api = Api(blueprint,
 
 api.add_namespace(user_ns, path='/api/users')
 api.add_namespace(login_ns, path='/api/login')
+
+app = create_app(os.getenv('PROJECT_ENV') or 'dev')
+
+app.register_blueprint(blueprint)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+app.app_context().push()
--- manage.py
+++ manage.py
@@ -1,29 +1,59 @@
 import os
 import unittest
+import sys
+import subprocess
 
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
-from flask_cors import CORS
+from flask_script import Manager, Option, Command
 
-from app import blueprint
-from app.main import create_app, db
+from app import app
+from app.main import db
 from app.main.model import user
 
-app = create_app(os.getenv('PROJECT_ENV') or 'dev')
-app.register_blueprint(blueprint)
-cors = CORS(app, resources={r"*": {"origins": "*"}})
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+HOST = '0.0.0.0'
+PORT = '8000'
 
-app.app_context().push()
+sys.path.insert(0, os.path.join(BASE_DIR, 'src'))
+
+# Originally from https://gist.github.com/menghan/9a632bbb0acb445f4f3a
+class GunicornServer(Command):
+    """Run the app within Gunicorn"""
+
+    def get_options(self):
+        from gunicorn.config import make_settings
+
+        settings = make_settings()
+        options = []
+
+        for setting, klass in settings.items():
+            if klass.cli:
+                if klass.const is not None:
+                    options.append(Option(*klass.cli, const=klass.const, action=klass.action))
+                else:
+                    options.append(Option(*klass.cli, action=klass.action))
+
+        return options
+
+    def run(self, *args, **kwargs):
+        run_args = sys.argv[2:]
+        run_args.append('-b '+HOST+':'+PORT)
+        run_args.append('--reload')
+        run_args.append('--log-level=debug')
+        run_args.append('manage:app')
+        subprocess.Popen([os.path.join(os.path.dirname(sys.executable), 'gunicorn')] + run_args).wait()
+
 
 manager = Manager(app)
 
 migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', GunicornServer)
 
 @manager.command
 def run():
-    app.run(host='0.0.0.0',port=80)
+    app.run(host=HOST,port=PORT)
 
 @manager.command
 def test():
